7b455ac80be042a512403ad83dfe0496020cf3ef
prem <premshah7202@gmail.com>
2025-05-22 08:43:12 +0530

    Merge branch 'feature2'

--- src/Array/TwoPointers/RemoveDuplicate.java
+++ src/Array/TwoPointers/RemoveDuplicate.java
@@ -4,29 +4,38 @@ import java.util.Arrays;
 
 public class RemoveDuplicate {
     public static void main(String[] args) {
-        int[] arr = {1,2,2,3,3,4,5,5};
-        remove(arr);
+        int[] arr = {4,5,5};
+        removeDuplicateInPlace(arr);
     }
 
-    public static void remove(int[] arr) {
-        int i = 0;
-        int j = 1;
-        int el = 0;
+    public static int uniqueElementCount(int[] arr) {
+        if (arr.length == 0) {
+            return 0;
+        }
 
-        while (i < arr.length) {
-            if (el < arr[i]) {
-                el = arr[i];
-                i++;
-            }
-
-            if (arr[i] == el) {
-                j = i;
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
+        int count = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i-1]){
+                count++;
             }
         }
 
-        System.out.println(Arrays.toString(arr));
+        return count;
+    }
+    
+    public static void removeDuplicateInPlace(int[] arr) {
+        if (arr.length == 0) {
+            System.err.println("Empty Array");
+            return;
+        }
+
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        System.out.println(Arrays.toString(Arrays.copyOfRange(arr, 0, i+1)));
     }
 }
